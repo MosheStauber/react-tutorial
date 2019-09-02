b2a56e932b836b2839eef6464beb3fe925a3861d
Moshe <moshe.stauber89@gmail.com>
2019-09-02 01:11:35 -0400

    Updates

---
+++ precache-manifest.c42bde01f746529b567d5b71e3c7fbb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff777e0f54d68280a60f3f442557487",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "5f4c0d312d79c8fa9f8e",
+    "url": "/react-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "beecb53a3d988c0a2b71",
+    "url": "/react-test/static/js/2.6d2906ef.chunk.js"
+  },
+  {
+    "revision": "5f4c0d312d79c8fa9f8e",
+    "url": "/react-test/static/js/main.80311948.chunk.js"
+  },
+  {
+    "revision": "4564166d317078afebd0",
+    "url": "/react-test/static/js/runtime~main.c52377be.js"
+  }
+]);
